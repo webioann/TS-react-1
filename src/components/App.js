05ffc548789f8dc7f309f441d681a2306faab994
webioann <ioannn7777@gmail.com>
2022-05-26 12:32:46 +0300

    commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>Hello TS</div>
+    )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App.js'
+import store from "./Redux/store.js"
+import { Provider } from 'react-redux'
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
